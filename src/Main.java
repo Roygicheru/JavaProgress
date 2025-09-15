839a4b234629f8c46d78cc9fe82c98dea1ad3db4
roygicheru <gicheru.k.roy@gmail.com>
2025-09-15 11:46:32 +0300

    new changes to main file

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 //import JavaLists.JavaListsOne;
 import Area.Area;
-import JavaSprint.JavaSprint;
+// import JavaSprint.JavaSprint;
 
 public class Main {
     public static void main(String[] args) {
 
-            JavaSprint javaSprint = new JavaSprint();
-            javaSprint.mainProj();
+            // JavaSprint javaSprint = new JavaSprint();
+            // javaSprint.mainProj();
 
             Area area = new Area();
             area.calculateTriangleArea(); 
